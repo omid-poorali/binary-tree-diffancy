ecd5ccfff6b64062a9a592f74757210deea3c0bc
toranj-org <omidpoorali1992@gmail.com>
2023-03-03 23:25:38 +0330

    feat: change binary tree logic

--- src/pages/home/tree-visualizer/main.tsx
+++ src/pages/home/tree-visualizer/main.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import { binaryTree } from "utils";
+import { TreeNode } from "./tree-node";
+
 type PropsType = {
     data: string;
 }
 
 export const TreeVisualizer = (props: PropsType) => {
-    console.log(props.data)
+
+    const [tree, setTree] = useState(binaryTree(null));
+    const nodes = useRef<string[]>([]);
+
+    useEffect(() => {
+        nodes.current = props.data.split(" ");
+        const newTree = binaryTree(null);
+        nodes.current.forEach(node => {
+            if (node) newTree.insert(node)
+        });
+        setTree(() => newTree);
+
+    }, [nodes, props.data]);
+
+
+    if (!nodes.current.length) {
+        return (
+            <>placeholder</>
+        );
+    }
+
+
     return (
-        <div>Tree</div>
-    )
+        <TreeNode node={tree.toGraph()} />
+    );
+
+
 }
